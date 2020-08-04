b8768663e118f42e95bbc95719a4af7ff0cdb291
Raphael Balogo <balogoraphael@gmail.com>
2020-08-04 16:52:17 -0300

    Complete app but has prop drilling, needs refactor

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/Pages/About';
@@ -14,6 +15,8 @@ class App extends Component {
 
 	state = {
 		users: [],
+		user: {},
+		repos: [],
 		loading: false,
 		alert: null,
 	};
@@ -49,6 +52,34 @@ class App extends Component {
 		});
 	};
 
+	//Get user profile details
+	getUser = async username => {
+		this.setState({ loading: true });
+
+		const res = await axios.get(
+			`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+
+		this.setState({
+			user: res.data,
+			loading: false,
+		});
+	};
+
+	//Get user repos
+	getUserRepos = async username => {
+		this.setState({ loading: true });
+
+		const res = await axios.get(
+			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+
+		this.setState({
+			repos: res.data,
+			loading: false,
+		});
+	};
+
 	//Set alert if search is empty
 	setAlert = (message, type) => {
 		this.setState({
@@ -67,8 +98,8 @@ class App extends Component {
 	//------------------------------------------------------------------------------------------------------//
 	//Render
 	render() {
-		const { users, loading, alert } = this.state;
-		const { searchUsers, clearUsers, setAlert } = this;
+		const { users, loading, alert, user, repos } = this.state;
+		const { searchUsers, clearUsers, setAlert, getUser, getUserRepos } = this;
 
 		return (
 			<Router>
@@ -94,6 +125,20 @@ class App extends Component {
 								)}
 							/>
 							<Route exatc path='/about' component={About} />
+							<Route
+								exact
+								path='/user/:login'
+								render={props => (
+									<User
+										{...props}
+										getUser={getUser}
+										getUserRepos={getUserRepos}
+										user={user}
+										repos={repos}
+										loading={loading}
+									/>
+								)}
+							/>
 						</Switch>
 					</div>
 				</div>
